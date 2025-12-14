c3e870b3827a133f71ffa4b12ec26808b2a956a3
viveksingh2310 <viveksingh14538@gmail.com>
2025-12-14 20:02:57 +0530

    up CORS
    Updated the CORS policy to allow the frontend URL .

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://sweet-shop-mgt-assignment-2.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
